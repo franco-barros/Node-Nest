f099df9bae53cdb3fd6c2755fda8e43d4ddef7dc
franco-barros <fb6776928@gmail.com>
2025-01-21 10:10:24 -0300

    second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,38 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola mundo';
+  }
+  @Get('nuevo')
+  nuevoEndpoint() {
+    return 'Yo soy nuevo ';
+  }
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('brand') brand: string,
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 50,
+  ) {
+    return `products: limit=> ${limit} offset=>${offset} brand=>${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy un filter `;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and category ${id}`;
   }
 }
